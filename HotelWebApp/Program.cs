ad103696a217ea3acb3abeff3aecb9d53716cf19
Michal Chojna <m.chojna@icloud.com>
2026-04-28 17:29:44 +0200

    Add swagger

--- HotelWebApp/Program.cs
+++ HotelWebApp/Program.cs
@@ -5,7 +5,15 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment()) app.MapOpenApi();
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(opt =>
+    {
+        opt.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
+
+}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
