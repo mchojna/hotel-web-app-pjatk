fec59a570ddad746eb347103fb875d685f4bf724
Michal Chojna <m.chojna@icloud.com>
2026-04-28 16:18:58 +0200

    Update models

--- HotelWebApp/Repository/ReservationRepository.cs
+++ HotelWebApp/Repository/ReservationRepository.cs
@@ -6,5 +6,5 @@ namespace HotelWebApp.Repository;
 // Reservation repository mockup
 public static class ReservationRepository
 {
-    public static List<Reservation> Reservations { get; }= DataInitializer.InitializeReservations();
+    public static List<Reservation> Reservations { get; } = DataInitializer.InitializeReservations();
 }
--- HotelWebApp/Repository/RoomRepository.cs
+++ HotelWebApp/Repository/RoomRepository.cs
@@ -6,5 +6,5 @@ namespace HotelWebApp.Repository;
 // Room repository mockup
 public static class RoomRepository
 {
-    public static List<Room> Rooms { get; }= DataInitializer.InitializeRooms();
+    public static List<Room> Rooms { get; } = DataInitializer.InitializeRooms();
 }
